26d044fe355193a44e771d009010c22ff8387d9f
BengisuAtar <bengisuatar1@gmail.com>
2024-11-20 20:07:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+
+    static public string ogrenci(string ad,string soyad)
+    {
+        Console.WriteLine("ad");
+      ad=Console.ReadLine();
+        soyad=Console.ReadLine();
+        return ad;
+      return soyad;
+    }
+    static public int Hesapla(int not1,int not2)
+    {
+        not1= Convert.ToInt16(Console.ReadLine());
+        not2 = Convert.ToInt16(Console.ReadLine());
+      int ort = (not1 + not2) / 2;
+        return ort;
+    }
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(ogrenci); 
+        Console.WriteLine(Hesapla); 
+        Console.ReadKey();
+    }
+}
