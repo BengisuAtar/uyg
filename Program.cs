80cfdf34f2f700620fd93173f7111cff1140998b
BengisuAtar <bengisuatar1@gmail.com>
2024-11-20 20:10:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        int toplam = 0;
+
+        for (int i = 1; i <= sayi; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine("sayıların toplamı="+toplam);
+        Console.ReadKey();
+    }
+}
