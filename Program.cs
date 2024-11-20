253f8580818bddf16619c1cf1eaf609fa569a72a
BengisuAtar <bengisuatar1@gmail.com>
2024-11-20 20:02:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int adi;
+int soyadi;
+
+for (int i = 0; i > 3; i++)
+{
+    Console.WriteLine("yaz"+i);
+    Console.WriteLine("yaz" + i);
+    Console.WriteLine("yaz" + i);
+    break;
+}
+
+Console.ReadKey();
