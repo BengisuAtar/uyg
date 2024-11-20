badd3787579698fe69d6c25119b52508a6517b23
BengisuAtar <bengisuatar1@gmail.com>
2024-11-20 20:11:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { Console.Write("Sayıyı Girin : ");
+        int sayi=Convert.ToInt32(Console.ReadLine());
+
+        int toplam = 0;
+        int osayi = sayi;
+        int bassayısı=osayi.ToString().Length;
+        while (sayi > 0)
+        {
+            int digit = sayi % 10; 
+            toplam += (int)Math.Pow(digit, bassayısı);
+            sayi /= 10; 
+        }
+
+        // Sonucu kontrol et
+        if (toplam == osayi)
+        {
+            Console.WriteLine(osayi+"r Armstrong sayısıdır.");
+        }
+        else
+        {
+            Console.WriteLine(osayi+"irrmstrong sayısı değildir.");
+        }
+        Console.ReadKey();
+    }
+}
