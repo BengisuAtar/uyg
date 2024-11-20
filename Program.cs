56c55792cf86a6e12adcba2566cf2e45ec7b2b68
BengisuAtar <bengisuatar1@gmail.com>
2024-11-20 20:10:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sayi, bolentoplam;
+        Console.WriteLine("sayı giriniz");
+        sayi=Convert.ToInt32(Console.ReadLine());
+       
+        bolentoplam = 0;
+        for (int i = 1; i < sayi; i++)
+        {
+            if (sayi%i==0)
+            {
+                 bolentoplam+=i;
+            }
+        }
+        if (bolentoplam == sayi)
+        {
+            Console.WriteLine("süper sayıdır");
+        }
+        else {
+            Console.WriteLine("süper sayı değildir");
+        }
+
+    }
+}
