1f3fa53143af2ddba3a977fb9c71f97113aec2d3
BengisuAtar <bengisuatar1@gmail.com>
2024-11-20 20:01:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int not1, not2, not3;
+Console.Write("1.Notu Giriniz:");
+not1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2.Notu Giriniz:");
+not2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("3.Notu Giriniz:");
+not3 = Convert.ToInt32(Console.ReadLine());
+
+int ortalama = (not1 + not2 + not3) / 3;
+switch(ortalama){
+    case (>=50): Console.Write("GEÇTİNİZ");
+        break;
+    case (<50):
+        Console.Write("KALDINIZ");
+        break;
+    default:
+        Console.WriteLine("Lütfen notları giriniz");
+        break;
+}
+
+
+Console.ReadKey();
